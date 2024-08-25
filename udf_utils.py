4ec7c4d804916478e4e8b6e735827fa2667fe045
KuldipDongare17 <kuldipdongare17@gmail.com>
2024-08-25 11:50:48 +0530

    first commit

---
+++ udf_utils.py
@@ -0,0 +1,38 @@
+def extract_file_name():
+    pass
+
+def extract_position():
+    pass
+
+def extract_class_code():
+    pass
+
+def extract_start_date():
+    pass
+
+def extract_end_date():
+    pass
+
+def extract_salary():
+    pass
+
+def extract_requirements():
+    pass
+
+def extract_notes():
+    pass
+
+def extract_duties():
+    pass
+
+def extract_selection():
+    pass
+
+def extract_experience_length():
+    pass
+
+def extract_eduation_length():
+    pass
+
+def extract_appication_location():
+    pass
